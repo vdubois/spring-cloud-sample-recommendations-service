f6f2aa7d7b07431e15756aeeb3e54f93c6ac5e99
Vincent Dubois <dubois.vct@free.fr>
2016-12-10 18:02:36 +0100

    [FIX] All poms and autoconfigurations

--- src/main/java/io/github/vdubois/SpringCloudSampleRecommendationsServiceApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleRecommendationsServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableCaching
 public class SpringCloudSampleRecommendationsServiceApplication {
