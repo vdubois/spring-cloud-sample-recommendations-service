b89122270928fe9c792d39ec870ba356e5c0b54f
Vincent Dubois <dubois.vct@free.fr>
2016-12-26 14:00:20 +0100

    [FIX] Hazelcast activation with property

--- src/main/java/io/github/vdubois/config/CacheConfiguration.java
+++ src/main/java/io/github/vdubois/config/CacheConfiguration.java
@@ -3,6 +3,7 @@ package io.github.vdubois.config;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CacheConfiguration {
 
+    @ConditionalOnProperty(value = "hazelcast.enabled", havingValue = "true")
     @Bean
     CacheManager cacheManager() {
         return new HazelcastCacheManager(hazelcastInstance());
     }
 
+    @ConditionalOnProperty(value = "hazelcast.enabled", havingValue = "true")
     @Bean
     HazelcastInstance hazelcastInstance() {
         return HazelcastClient.newHazelcastClient();
